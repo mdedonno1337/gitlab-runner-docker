6d52236a96416d348548111bf056e7bec39232ed
Marco De Donno <Marco.DeDonno@unil.ch>
2018-05-22 01:09:57 +0200

    Refactoring of the entrypoint.sh script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,29 +1,18 @@
 #!/bin/bash
 
-trap '/tmp/exiting.sh' 0
-
-exec /tmp/register.sh &
-
 DATA_DIR="/etc/gitlab-runner"
 CONFIG_FILE=${CONFIG_FILE:-$DATA_DIR/config.toml}
 
-CA_CERTIFICATES_PATH=${CA_CERTIFICATES_PATH:-$DATA_DIR/certs/ca.crt}
-LOCAL_CA_PATH="/usr/local/share/ca-certificates/ca.crt"
+# Trap exit-codes send by Docker
+trap '/tmp/exiting.sh' 0
 
-update_ca() {
-  echo "Updating CA certificates..."
-  cp "${CA_CERTIFICATES_PATH}" "${LOCAL_CA_PATH}"
-  update-ca-certificates --fresh >/dev/null
-}
+# Startup script to register the worker to the gitlab server
+exec /tmp/register.sh &
 
-if [ -f "${CA_CERTIFICATES_PATH}" ]; then
-  # update the ca if the custom ca is different than the current
-  cmp --silent "${CA_CERTIFICATES_PATH}" "${LOCAL_CA_PATH}" || update_ca
-fi
-
-# launch gitlab-ci-multi-runner passing all arguments
+# Run the gitlab-runner in background, and watch the PID
 gitlab-runner run --user=gitlab-runner --working-directory=/home/gitlab-runner &
 BACK_PID=$!
 wait $BACK_PID
 
+# Clean the worker (unregister and stuff)
 /tmp/exiting.sh > /dev/null 2>&1
